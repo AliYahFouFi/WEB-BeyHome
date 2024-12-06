d554dfa60aafb0c3a2970b3231bde9673758e108
Zulfikaryehya <zyehya007@gmail.com>
2024-12-07 00:29:15 +0200

    bookingandmore

--- BeyHome/resources/views/home.blade.php
+++ BeyHome/resources/views/home.blade.php
@@ -16,33 +16,3 @@
 
 <hr>
 <hr>
-<form action="{{ route('bookings.store') }}" method="POST">
-    @csrf
-    <div class="form-group">
-        <label for="property_id">Property</label>
-        <select name="property_id" id="property_id" class="form-control" required>
-            @foreach ($properties as $property)
-                <option value="{{ $property->id }}">{{ $property->name }}</option>
-            @endforeach
-        </select>
-    </div>
-
-    <input type="hidden" name="user_id" value="{{ Auth::id() }}">
-
-    <div class="form-group">
-        <label for="start_date">Start Date</label>
-        <input type="date" name="start_date" id="start_date" class="form-control" required>
-    </div>
-
-    <div class="form-group">
-        <label for="end_date">End Date</label>
-        <input type="date" name="end_date" id="end_date" class="form-control" required>
-    </div>
-
-    <div class="form-group">
-        <label for="total_price">Total Price</label>
-        <input type="number" name="total_price" id="total_price" class="form-control" required>
-    </div>
-
-    <button type="submit" class="btn btn-primary">Book Now</button>
-</form>
